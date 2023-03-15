4c185548dd36b96f636a7a7c7772886f11dc6c58
Kamal Mango <kamal.mango7@gmail.com>
2023-03-15 15:03:04 +0100

    modify

--- index.js
+++ index.js
@@ -1,65 +1,63 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const OneSignal = require('onesignal-node');
-
+const OneSignal = require('@onesignal/node-onesignal');
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Replace with your OneSignal configuration
+const onesignalAppId = '58565960-d135-41b4-b90b-a139f385b537';
+const onesignalApiKey = process.env.onesignal_api_key;
 
-// Set up OneSignal client
-const oneSignalClient = new OneSignal.Client({
-  app: { appAuthKey: process.env.onesignal_api_key, appId: '58565960-d135-41b4-b90b-a139f385b537' },
+const configuration = OneSignal.createConfiguration({
+  authMethods: {
+    app_key: {
+      tokenProvider: {
+        getToken() {
+          return onesignalApiKey;
+        },
+      },
+    },
+  },
 });
 
-// Route to handle incoming requests from iOS device
-app.post('/', async (req, res) => {
-  // Get the device's APNS token from the request headers
-  const apnsToken = req.headers['apns-token'];
+const client = new OneSignal.DefaultApi(configuration);
 
-  // Check if the device is already registered
-  const existingDevice = await oneSignalClient.viewDevices({ app_id: '58565960-d135-41b4-b90b-a139f385b537', push: { include_external_user_ids: [apnsToken] } });
-
-  if (!existingDevice || existingDevice.length === 0) {
-    // If the device is not registered, add it to OneSignal using the APNS token as the external user ID
-    const newDevice = new OneSignal.Device({ 
-      app_id: '58565960-d135-41b4-b90b-a139f385b537', 
-      identifier: apnsToken,
-      external_user_id: apnsToken,
-      device_type: 0 // 0 is for iOS devices
-    });
-    
-    try {
-      const result = await oneSignalClient.addDevice(newDevice);
-      console.log('Device added:', result);
-    } catch (err) {
-      console.error('Error adding device:', err);
-      res.status(500).send('Error adding device');
-      return;
-    }
-  }
-
-  // Create a notification object to send
-  const notification = {
-    headings: { en: 'Iris AI' },
-    contents: { en: 'Your video is ready to download!' },
-    ios_badgeType: 'Increase',
-    ios_badgeCount: 1,
-    include_external_user_ids: [apnsToken],
-  };
-
-  // Send the notification using the OneSignal API
+app.post('/send-notification/:callback_device_id', async (req, res) => {
   try {
-    const result = await oneSignalClient.createNotification(notification);
-    console.log('Notification sent:', result);
-    res.status(200).send('Notification sent!');
-  } catch (err) {
-    console.error('Error sending notification:', err);
-    res.status(500).send('Error sending notification');
+    const callbackDeviceId = req.params.callback_device_id;
+
+    // Get all registered players
+    const playersResponse = await client.getPlayers(onesignalAppId);
+
+    // Check if the callback device ID exists
+    const existingPlayer = playersResponse.players.find(player => player.device_id === callbackDeviceId);
+
+    let playerId;
+
+    if (existingPlayer) {
+      playerId = existingPlayer.id;
+    } else {
+      // Create a new player if not found
+      const newPlayer = new OneSignal.Player();
+      newPlayer.device_type = 1; // For iOS devices
+      newPlayer.app_id = onesignalAppId;
+      newPlayer.device_id = callbackDeviceId;
+
+      const createPlayerResponse = await client.createPlayer(newPlayer);
+      playerId = createPlayerResponse.id;
+    }
+
+    // Send notification to the found or newly created player
+    const notification = new OneSignal.Notification();
+    notification.app_id = onesignalAppId;
+    notification.include_player_ids = [playerId];
+    notification.contents = { en: 'Hello from Node.js server!' };
+
+    const notificationResponse = await client.createNotification(notification);
+    res.status(200).json({ success: true, response: notificationResponse });
+  } catch (error) {
+    console.error('Error sending notification:', error);
+    res.status(500).json({ success: false, error: error.message });
   }
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
